7242e2f040b1fb4326ae6f50f8ebeb952fec4c42
davidashton0629 <77017046+davidashton0629@users.noreply.github.com>
2021-01-08 09:34:03 -0500

    Create basic.funcs.js

---
+++ basic.funcs.js
@@ -0,0 +1,41 @@
+function getClasses(obj)
+{
+	var classList = $(obj).attr("class");
+	var classes = classList.split(" ");
+	return classes;
+}
+
+function convertToSlug(Text)
+{
+    return Text
+        .toLowerCase()
+        .replace(/ /g,'-')
+        .replace(/[^\w-]+/g,'')
+        ;
+}
+
+function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+}
+	
+function isVisible(elem)
+{
+	var docViewTop = $(window).scrollTop();
+	var docViewBottom = docViewTop + $(window).height();
+
+	var elemTop = $(elem).offset().top;
+	var elemBottom = elemTop + $(elem).height();
+
+	return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
